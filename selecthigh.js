6a644c13e5e5f86188b352a7906793158ad30338
EunsisTech <Eunsistech@gmail.com>
2024-02-05 21:55:36 -0500

    the first playable version

--- selecthigh.js
+++ selecthigh.js
@@ -1,53 +1,110 @@
-const cards = require("./cards");
+const CARD_MOVEBY = 80;
 
 //Tell the library which element to use for the table
 cards.init({
 	table:'#card-table', 
-	type:NUMONLY
+	cardUrl: 'image/cards.png',
+	cardSize:{
+		width: 69,
+		height: 94,
+		padding: 100,
+	},
+	type:NUMONLY // Number only = No J, Q, K
 });
 
 //Create a new deck of cards
 let deck = new cards.Deck();
-let upperhand;
-let lowerhand;
-const defaultCard = new Card('bj', 0, 'void');
-let selectedCard=defaultCard
+let upperhand = new cards.Hand({faceUp:false, y:60});
+let lowerhand = new cards.Hand({faceUp:true,  y:340});
+// defaultCard is invalid card in this game
+const defaultCard = new cards.Card('bj', 0, 'void');
+let selectedCard=defaultCard;
+let upperhighcard;
 
 const init = () => {
-	deck.removeCard(cards.all);
+	$('#btnCheck').hide();
+	$('#btnNewGame').hide();
+	$('.labels').hide();
+	$('#btnDeal').show();
+	
 	//cards.all contains all cards, put them all in the deck
+	deck.removeCard(cards.all);
 	deck.addCards(cards.all);
+	deck.x = 250;
 	cards.shuffle(deck);
 	//No animation here, just get the deck onto the table.
 	deck.render({immediate:true});
-
-	//Now lets create a couple of hands, one face down, one face up.
-	upperhand = new cards.Hand({faceUp:false, y:60});
-	lowerhand = new cards.Hand({faceUp:true,  y:340});
 }
 
-//Let's deal when the Deal button is pressed:
-$('#deal').click(function() {
-	//Deck has a built in method to deal to hands.
-	$('#deal').hide();
-	deck.deal(2, [upperhand, lowerhand], 50);
+//Let's deal when the btnDeal button is pressed:
+$('#btnDeal').click(function() {
+	//Deck has a built in method to btnDeal to hands.
+	$('#btnDeal').hide();
+	$('#btnCheck').show();
+	$('#btnNewGame').show();
+	// move deck to left
+	deck.x -= 100;
+	deck.render({immediate:true});
+	
+	// initialize selectedCard;
 	selectedCard=defaultCard;
+	
+	// btnDeal 2 cards and select higher card for upper deck
+	deck.deal(2, [upperhand, lowerhand], 50,()=>{
+		console.debug(upperhand);
+		upperhighcard = upperhand[0].rank > upperhand[1].rank ? upperhand[0]:upperhand[1];
+		upperhighcard.moveBy(0,CARD_MOVEBY,50);
+	});
 });
 
-//Finally, when you click a card in your hand, if it's
-//the same suit or rank as the top card of the discard pile
-//then it's added to it
 lowerhand.click(function(card){
 	// user cancel the selection(click the selected card, again)
 	if (card === selectedCard)
 	{
 		selectedCard=defaultCard;
-		card.moveTo(card.x, card.y+50);
+		card.moveBy(0, CARD_MOVEBY, 50);
 	}
 	else
 	{
+		if (selectedCard != defaultCard ) 
+		{
+			selectedCard.moveBy(0,CARD_MOVEBY,50);
+		}
 		selectedCard=card;
-		card.moveTo(card.x, card.y-50);
+		card.moveBy(0,CARD_MOVEBY*-1, 50);
 	}
 });
 
+$('#btnCheck').click(function() {
+	if (selectedCard===defaultCard)
+	{
+		$('#lblSelectOne').text("Pick one of cards!").show();
+		return;
+	}
+	$('#btnCheck').hide();
+	$('#lblSelectOne').hide();
+	$('#btnNewGame').show();
+
+	// show upperhand
+	for (var i=0; i<upperhand.length;i++)
+	{
+		upperhand[i].showCard();
+	}
+
+	var winorlose="Win !!";
+	if (upperhighcard.rank>selectedCard.rank)
+	{
+		winorlose="Lose !!"
+	}
+	else if (upperhighcard.rank===selectedCard.rank)
+	{
+		winorlose="Draw"
+	}
+	$('#lblWinOrLose').text(winorlose).show();
+});
+
+$('#btnNewGame').click(function() {
+	init();
+});
+
+init();
